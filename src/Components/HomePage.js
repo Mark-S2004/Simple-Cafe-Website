ab3b05550247df5736102a55a115a3199d342b4b
Mark Saleh <mark-saleh@outlook.com>
2023-05-03 17:17:41 +0300

    Add home page component

---
+++ src/Components/HomePage.js
@@ -0,0 +1,12 @@
+import logo from "../images/starbucks_logo.jpeg"
+
+const HomePage = () => {
+    return (
+        <main>
+            <img src={logo} alt="Logo" width={"100px"}/>
+            <h1>Cafe</h1>
+        </main>
+    )
+}
+
+export default HomePage
