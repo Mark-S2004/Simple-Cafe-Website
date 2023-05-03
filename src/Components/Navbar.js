3fbdde689735a3a23d5386d14b1b545168cb5989
Mark Saleh <mark-saleh@outlook.com>
2023-05-03 17:18:06 +0300

    Add navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,24 @@
-import {Link} from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom'; 
 
 const NavBar = () => {
     return (
-        <nav>
-            <Link to="\">HomePage</Link>
-            <Link to="\menu">Menu</Link>
-            <Link to="\about-us">About us</Link>
-            <Link to="\contact">Contact</Link>
-        </nav>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to={"/"}>React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                        <Nav.Link as={Link} to={"/menu"}>Menu</Nav.Link>
+                        <Nav.Link as={Link} to={"/about-us"}>About Us</Nav.Link>
+                        <Nav.Link as={Link} to={"/contact"}>Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+    </Navbar>
     )
 }
 
