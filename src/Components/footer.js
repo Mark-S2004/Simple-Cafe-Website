d895fa489545e548f946086e1c925b5895d1085f
Mark Saleh <mark-saleh@outlook.com>
2023-05-03 17:17:11 +0300

    Add footer component

---
+++ src/Components/footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer>
+            <address>
+                <a href="https://twitter.com/MarkBotros_" title="twitter account"><i className="fa-brands fa-twitter"></i></a>
+                <a href="https://www.facebook.com/mark.saleh.75" title="facebook account"><i className="fa-brands fa-facebook"></i></a>
+                <a href="https://github.com/Mark-S2004" title="pinterest account"><i className="fa-brands fa-pinterest"></i></a>
+            </address>
+            <small>© All rights reserved | Mark Saleh</small>
+        </footer>
+    )
+}
+
+export default Footer
