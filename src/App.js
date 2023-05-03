4ff442198b116cded23f4b03ceb9f38daed5e897
Mark Saleh <mark-saleh@outlook.com>
2023-05-03 17:19:39 +0300

    Route all components in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import NavBar from './Components/Navbar';
+import NavBar from './Components/NavBar';
+import AboutUs from './Components/AboutUs';
+import Footer from './Components/footer';
+import Menu from './Components/Menu';
+import Contact from './Components/Contact';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Routes>
-        
-      </Routes>
+      <header><NavBar/></header>
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+          <Route path='/menu' element={<Menu/>}></Route>
+          <Route path='/about-us' element={<AboutUs/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+        </Routes>
+      <Footer/>
     </div>
   );
 }
