2a0a3d14bbcb8e31019d5f596fc250d1beb75692
Mark Saleh <mark-saleh@outlook.com>
2023-05-17 09:25:10 +0300

    Add "My Cafe" name as brand for NavBar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand as={Link} to={"/"}>React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand as={Link} to={"/"}>My Cafe</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
